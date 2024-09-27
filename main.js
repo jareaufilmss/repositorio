a5a1f6c20a46d0ada5d66bfb76a2b677a7c0e692
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-09-27 11:55:10 -0300

    h

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(6));
+
+let media=function(valor1, valor2, valor3){
+    return valor1+valor2+valor3(/3);
+}
+console.log();
