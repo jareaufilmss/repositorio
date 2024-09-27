6363fa145c07f6ea9878ef5710127c74119ed178
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-09-27 12:00:14 -0300

    n

--- main.js
+++ main.js
@@ -11,4 +11,4 @@ console.log(areaQuadrado(6));
 let media=function(valor1, valor2, valor3, valor4){
     return valor1+valor2+valor3/(valor4);
 }
-console.log(media(1, 2, 3, 3))
+console.log(media(2, 2, 3, 3))
