3943903b586aa9d3bff4ade22c6bd46fe090c9ba
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-10-04 12:04:37 -0300

    h

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ let estados=['PR','SC','RS','SP','RJ','MG','ES','MS','GO','MT','BA','SE','AL','P
 // adilene++;
 // }
 for (let rafael = 0; rafael < estados.length; rafael++) {
+    console.log(rafael);
     console.log(estados[rafael]);
     
 }
