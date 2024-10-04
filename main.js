ab7193afe63d5c52ba48d5054d0906380e78c8fc
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-10-04 11:21:31 -0300

    f

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-function soma(numero1, numero2){
-    return numero1+numero2;
-}
-console.log(soma(6,4));
+// function soma(numero1, numero2){
+//     return numero1+numero2;
+// }
+// console.log(soma(6,4));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
 
-let media=function(valor1, valor2, valor3){
-    return (valor1+valor2+valor3)/3
-}
+// let media=function(valor1, valor2, valor3){
+//     return (valor1+valor2+valor3)/3
+// }
+// console.log(media(1, 2, 3));
+
+// let valor1=parseInt(prompt(Qual sua nota?));
+// let valor2=parseInt(prompt(Qual sua nota?));
+// let valor3=pareInt(prompt(Qual sua nota?));
+
+// function media (x, y, z){
+//     return (x+y+z)/3
+// }
+// document.write (media(valor1, valor2, valor3));
+
+let estados=['PR','SC','RS','SP','RJ','MG','ES','MS','GO','MT','BA','SE','AL','PE','PB','RN','CE','PI','MA','TO','PA','AP','RR','AM','AC','RO','DF']
