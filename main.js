5433f6cc39d06abc5a715031aa5ea12dfa20c839
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-10-04 12:23:11 -0300

    h

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ let estados=['PR','SC','RS','SP','RJ','MG','ES','MS','GO','MT','BA','SE','AL','P
 //     console.log(estados[rafael]);
     
 // }
-let ana=parseInt(prompt(Qual a sua idade?));
+let ana=parseInt(prompt('Qual a sua idade?'));
 if(ana>17){
     console.log('pode dirigir');
 
