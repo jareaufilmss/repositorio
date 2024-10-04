54a3e848e4a0df947932a7afe366fba2703f9757
gabi <140652360+jareaufilmss@users.noreply.github.com>
2024-10-04 11:03:44 -0300

    j

--- main.js
+++ main.js
@@ -11,7 +11,3 @@ console.log(areaQuadrado(6));
 let media=function(valor1, valor2, valor3){
     return (valor1+valor2+valor3)/3
 }
-console.log(media(1, 2, 3))
-
-let media2=parseInt(prompt('Qual as suas notas?'));
-document.write(media)
